67fab23723bdd0aa7a26043abcb9d68b7d0b9236
muxirr <muxirr@qq.com>
2025-01-21 17:14:39 +0800

    第一天：游戏基础框架搭建

---
+++ include/atlas.h
@@ -0,0 +1,51 @@
+#ifndef _ALTAS_H_
+#define _ALTAS_H_
+
+#include "SDL_image.h"
+
+#include <vector>
+
+class Atlas {
+public:
+  Atlas() = default;
+
+  ~Atlas() {
+    for (auto texture : tex_list) {
+      SDL_DestroyTexture(texture);
+    }
+  }
+
+  void load(SDL_Renderer *renderer, const char *path_template, int num) {
+    this->tex_list.clear();
+    this->tex_list.resize(num);
+
+    char path[256];
+
+    for (int i = 0; i < num; i++) {
+      sprintf_s(path, path_template, i + 1);
+      SDL_Texture *texture = IMG_LoadTexture(renderer, path);
+      tex_list.push_back(texture);
+    }
+  }
+
+  void clear() {
+    for (auto texture : tex_list) {
+      SDL_DestroyTexture(texture);
+    }
+    this->tex_list.clear();
+  }
+
+  int get_size() { return static_cast<int>(this->tex_list.size()); }
+
+  SDL_Texture *get_image(int index) {
+    if (index < 0 || index >= tex_list.size())
+      return nullptr;
+    return this->tex_list[index];
+  }
+  void add_image(SDL_Texture *texture) { this->tex_list.push_back(texture); }
+
+private:
+  std::vector<SDL_Texture *> tex_list;
+};
+
+#endif // _ALTAS_H_
